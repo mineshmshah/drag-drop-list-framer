9ceee1a902489fddce5d45240feeb4374a68e682
Minesh <mineshmshah@hotmail.co.uk>
2019-09-11 12:04:57 +0100

    Updates

---
+++ precache-manifest.c0f83a446a22abafc1665f596c1ab540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "edec8fca38d93f39bf30",
+    "url": "/static/js/main.edec8fca.chunk.js"
+  },
+  {
+    "revision": "51c6f5b4206e721f335f",
+    "url": "/static/js/1.51c6f5b4.chunk.js"
+  },
+  {
+    "revision": "edec8fca38d93f39bf30",
+    "url": "/static/css/main.0a2e5277.chunk.css"
+  },
+  {
+    "revision": "2b9944bb92eaa2d24a2827147cf5e620",
+    "url": "/index.html"
+  }
+];
